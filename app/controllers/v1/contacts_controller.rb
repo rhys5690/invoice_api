ef2b751f832c955a9dd6d18386cee6425f0823b4
rhys5690 <rhys5690@gmail.com>
2018-02-14 08:55:39 +1100

    began adding authentication and sessions

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -3,6 +3,27 @@ class V1::ContactsController < ApplicationController
     @contact = Contact.all
 
     render json: @contact, status: :ok
-    
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    @onctact.save
+    render json @contact, status :created
+  end
+
+  def destroy
+    @contact = Contact.where(id: params[:id]).first
+    if @contact.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email)
   end
 end

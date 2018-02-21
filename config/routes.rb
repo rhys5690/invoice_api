306da0c764a224f19f719df9e53ea28e9190b388
rhys5690 <rhys5690@gmail.com>
2018-02-22 08:36:01 +1100

    created destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :v1 do
     resources :contacts
-    resources :sessions, only: [:create, :destroy]
+    resource :sessions, only: [:create, :destroy]
   end
 end
